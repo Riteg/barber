95dbd925a4877b1fc99e8613c6da96810652ece8
Yusuf Ziya Kalyoncu <yusuf@new.com.tr>
2023-03-13 16:18:07 +0300

    ios safe area fix

--- screens/TabNavigator.js
+++ screens/TabNavigator.js
@@ -3,17 +3,22 @@ import Profile from "./Tabs/Profile";
 import History from "./Tabs/History";
 import Settings from "./Tabs/Settings";
 import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
-import { FontAwesome5,MaterialIcons,FontAwesome,Ionicons } from '@expo/vector-icons';
+import { FontAwesome5, MaterialIcons, FontAwesome, Ionicons } from '@expo/vector-icons';
 import MapStack from "./MapStack"
+import { SafeAreaView, View } from "react-native";
 const Tab = createBottomTabNavigator();
 
 export default function TabNavigator({ navigation }) {
   return (
-    <Tab.Navigator screenOptions={({ route }) => ({tabBarShowLabel:false,headerShown: false,tabBarLabelStyle:{fontSize:14},tabBarActiveBackgroundColor:"#262626",tabBarActiveTintColor:"#d90",tabBarStyle: {height: 50,paddingHorizontal: 0,paddingTop: 0,backgroundColor: '#181818',position: 'absolute',borderTopWidth: 0,}})} >
-    <Tab.Screen name="Home" component={MapStack}options={{tabBarIcon: ({ color, size }) => (<FontAwesome5 name="cut" size={26} color="white" />)}}/>
-    <Tab.Screen name="Geçmiş" component={History} options={{tabBarIcon: ({ color, size }) => (<MaterialIcons name="date-range" size={26} color="white" />)}}/>
-    <Tab.Screen name="Profile" component={Profile} options={{tabBarIcon: ({ color, size }) => (<FontAwesome5 name="user" size={28} color="white" />)}}/>
-    <Tab.Screen name="Settings" component={Settings} options={{tabBarIcon: ({ color, size }) => (<FontAwesome name="gear" size={28} color="white" />)}}/>
-  </Tab.Navigator>
+    <View style={{ flex: 1, backgroundColor: '#181818' }} >
+      <SafeAreaView style={{ flex: 1 }} >
+        <Tab.Navigator screenOptions={({ route }) => ({ tabBarShowLabel: false, headerShown: false, tabBarLabelStyle: { fontSize: 14 }, tabBarActiveBackgroundColor: "#262626", tabBarActiveTintColor: "#d90", tabBarStyle: { height: 50, paddingHorizontal: 0, paddingTop: 0, backgroundColor: '#181818', position: 'absolute', borderTopWidth: 0, } })} >
+          <Tab.Screen name="Home" component={MapStack} options={{ tabBarIcon: ({ color, size }) => (<FontAwesome5 name="cut" size={26} color="white" />) }} />
+          <Tab.Screen name="Geçmiş" component={History} options={{ tabBarIcon: ({ color, size }) => (<MaterialIcons name="date-range" size={26} color="white" />) }} />
+          <Tab.Screen name="Profile" component={Profile} options={{ tabBarIcon: ({ color, size }) => (<FontAwesome5 name="user" size={28} color="white" />) }} />
+          <Tab.Screen name="Settings" component={Settings} options={{ tabBarIcon: ({ color, size }) => (<FontAwesome name="gear" size={28} color="white" />) }} />
+        </Tab.Navigator>
+      </SafeAreaView>
+    </View>
   );
 }
