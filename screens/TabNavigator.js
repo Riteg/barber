7b9a0486924b5672fffb6760f925649043c1b8b1
Yusuf Ziya Kalyoncu <yusuf@new.com.tr>
2023-04-04 01:33:25 +0300

    fix

--- screens/TabNavigator.js
+++ screens/TabNavigator.js
@@ -10,11 +10,12 @@ const Tab = createBottomTabNavigator();
 
 export default function TabNavigator({ navigation }) {
   return (
-    <Tab.Navigator screenOptions={({ route }) => ({ tabBarShowLabel: false, headerShown: false, tabBarLabelStyle: { fontSize: 14 }, tabBarActiveBackgroundColor: "#262626", tabBarActiveTintColor: "#d90", tabBarStyle: { height: 50, paddingHorizontal: 0, paddingTop: 0, backgroundColor: '#181818', position: 'absolute', borderTopWidth: 0, } })} >
-      <Tab.Screen name="Home" component={MapStack} options={{ tabBarIcon: ({ color, size }) => (<FontAwesome5 name="cut" size={26} color="white" />) }} />
-      <Tab.Screen name="Geçmiş" component={History} options={{ tabBarIcon: ({ color, size }) => (<MaterialIcons name="date-range" size={26} color="white" />) }} />
-      <Tab.Screen name="Profile" component={Profile} options={{ tabBarIcon: ({ color, size }) => (<FontAwesome5 name="user" size={28} color="white" />) }} />
-      <Tab.Screen name="Settings" component={Settings} options={{ tabBarIcon: ({ color, size }) => (<FontAwesome name="gear" size={28} color="white" />) }} />
-    </Tab.Navigator>
+    <SafeAreaView style={{ flex: 1, backgroundColor: '#181818' }}>
+      <Tab.Navigator screenOptions={({ route }) => ({ tabBarShowLabel: false, headerShown: false, tabBarLabelStyle: { fontSize: 14 }, tabBarActiveBackgroundColor: "#262626", tabBarActiveTintColor: "#d90", tabBarStyle: { height: 50, paddingHorizontal: 0, paddingTop: 0, backgroundColor: '#181818', position: 'absolute', borderTopWidth: 0, } })} >
+        <Tab.Screen name="Home" component={MapStack} options={{ tabBarIcon: ({ color, size }) => (<FontAwesome5 name="cut" size={26} color="white" />) }} />
+        <Tab.Screen name="Geçmiş" component={History} options={{ tabBarIcon: ({ color, size }) => (<MaterialIcons name="date-range" size={26} color="white" />) }} />
+        <Tab.Screen name="Profile" component={Profile} options={{ tabBarIcon: ({ color, size }) => (<FontAwesome5 name="user" size={28} color="white" />) }} />
+      </Tab.Navigator>
+    </SafeAreaView>
   );
 }
